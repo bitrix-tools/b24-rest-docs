207ed076c0f45e17b5fedc6417b2fc2a75dc0ae4
Serg Vostrikov <sv@bitrix24.com>
2024-08-20 19:32:32 +0200

    Подключение метрики

--- _assets/b24addons.js
+++ _assets/b24addons.js
@@ -374,3 +374,28 @@ function initB24items() {
 initB24itemsIfReady();
 
 mainObserver.observe(document, { childList: true, subtree: true });
+
+window.onload = function() {
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){
+            (m[i].a=m[i].a||[]).push(arguments)
+        };
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) { 
+                return; 
+            }
+        }
+        k = e.createElement(t),
+        a = e.getElementsByTagName(t)[0];
+        k.async = 1;
+        k.src = r;
+        a.parentNode.insertBefore(k,a);
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(98117665, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true
+    });
+};
